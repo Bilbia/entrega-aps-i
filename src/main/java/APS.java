0b0eedd860e57dceb1a9d923fb0c98935082659c
Bilbia <biamcs2000@gmail.com>
2020-05-18 16:55:02 -0300

    norGate feito e colocada a foto no resc

--- src/main/java/APS.java
+++ src/main/java/APS.java
@@ -13,6 +13,7 @@ public class APS {
         model.add(new AndGate());
         model.add(new OrGate());
         model.add(new XorGate());
+        model.add(new NorGate());
 
         View view = new View(model);
 
